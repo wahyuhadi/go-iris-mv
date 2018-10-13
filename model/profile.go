c7d2156414283883aced38ea04691f9f6ee92ded
Rahmat Wahyu Hadi <hrahmatwahyu@gmail.com>
2018-10-13 13:54:02 +0700

    fixing golang style code and add comment befor function

--- model/profile.go
+++ model/profile.go
@@ -2,14 +2,15 @@ package model
 
 import "time"
 
+// Profile model
 type Profile struct {
 	ID        int64      `json:"id" gorm:"primary_key"`
-	UserID    int64      `json:"user_id, omitempty" gorm:"type:bigint REFERENCES users(id)"`
-	Address   string     `json:"address, omitempty" gorm:"not null; type:varchar(100)"`
-	LastName  string     `json:"lastname, omitempty" gorm:"not null; type:varchar(100)"`
-	FirstName string     `json:"firstname, omitempty" gorm:"not null; type:varchar(100)"`
-	CreatedAt *time.Time `json:"createdAt, omitempty"`
-	UpdatedAt *time.Time `json:"updatedAt, omitempty"`
-	DeletedAt *time.Time `json:"deletedAt, omitempty" sql:"index"`
+	UserID    int64      `json:"user_id,omitempty" gorm:"type:bigint REFERENCES users(id)"`
+	Address   string     `json:"address,omitempty" gorm:"not null; type:varchar(100)"`
+	LastName  string     `json:"lastname,omitempty" gorm:"not null; type:varchar(100)"`
+	FirstName string     `json:"firstname,omitempty" gorm:"not null; type:varchar(100)"`
+	CreatedAt *time.Time `json:"createdAt,omitempty"`
+	UpdatedAt *time.Time `json:"updatedAt,omitempty"`
+	DeletedAt *time.Time `json:"deletedAt,omitempty" sql:"index"`
 	//User      User       `gorm:"foreignkey:UserRefer"`
 }
--- model/user.go
+++ model/user.go
@@ -2,13 +2,14 @@ package model
 
 import "time"
 
+// User model
 type User struct {
 	ID        int64      `json:"id" gorm:"primary_key"`
-	Role      string     `json:"role, omitempty" gorm:"not null; type:ENUM('admin', 'user', 'root')"`
+	Role      string     `json:"role,omitempty" gorm:"not null; type:ENUM('admin', 'user', 'root')"`
 	Email     string     `json:"email" gorm:"not null; size:255"`
 	Password  string     `json:"password" gorm:"not null; size:255"` // Default size for string is 255, reset it with this tag
-	CreatedAt *time.Time `json:"createdAt, omitempty"`
-	UpdatedAt *time.Time `json:"updatedAt, omitempty"`
-	DeletedAt *time.Time `json:"deletedAt, omitempty" sql:"index"`
+	CreatedAt *time.Time `json:"createdAt,omitempty"`
+	UpdatedAt *time.Time `json:"updatedAt,omitempty"`
+	DeletedAt *time.Time `json:"deletedAt,omitempty" sql:"index"`
 	Profile   Profile    `json:"profile"` //`gorm:"foreignkey:UserID;association_foreignkey:Refer"` // One-To-Many relationship (has many - use Email's UserID as foreign key)
 }
