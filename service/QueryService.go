6bc5da16052cbe5b92923a2f159e6374e2da10e6
Mochamad Iqbal Dwi Cahyo <mochadwi@users.noreply.github.com>
2018-10-16 14:23:53 +0700

    Changelog: (#10)
    - Generate model using go-queryset for type-safe query
    - Update dependencies using dep ensure

--- service/QueryService.go
+++ service/QueryService.go
@@ -1,12 +1,16 @@
 package service
 
+import (
+	"fmt"
+	"github.com/mochadwi/go-iris-mv/config"
+	"github.com/mochadwi/go-iris-mv/model"
+)
+
 // ini untuk proses query
-func GetAll(Model interface{}) (interface{}, error) {
-	// db := config.GetDatabaseConnection()
-	// defer db.Close()
-	//var model []Model
-	//db.Find(&model)
-	//return model
-	var Modelel []Model
-	return model
+// ini get all harusnya return slice of interface, right?
+func GetAll(obj interface{}) (interface{}, error) {
+	db := config.GetDatabaseConnection()
+	defer db.Close()
+	db.Find(obj.(model.User))
+	return obj, fmt.Errorf("exception throws")
 }
